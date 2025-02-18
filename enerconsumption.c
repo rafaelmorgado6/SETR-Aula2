fdd1dbdcb2b4d84aeaa8325c6816e1bfe226405d
P-Ramos16 <p.ramos@ua.pt>
2025-02-18 18:52:38 +0000

    Added doxygen docs for the main file

--- enerconsumption.c
+++ enerconsumption.c
@@ -16,7 +16,8 @@ void ecAdd(float reading){
     if (currentIndex < MAX_READINGS) {
         readings[currentIndex++] = reading;
         printf("    -> Reading added: %.2f kW/h\n", reading);
-    } else {
+    } 
+    else {
         printf("ERROR: Readings limit reached.\n");
     }
 }
@@ -25,15 +26,21 @@ void dtSum(float *peak, float *dailyValue, float *dailyCost) {
     *peak = 0.0;
     *dailyValue = 0.0;
 
+    //  Process the values
     for (int i = 0; i < currentIndex; i++) {
+        //  Get peak value
         if (readings[i] > *peak) {
             *peak = readings[i];
         }
+
+        //  Add the current value to the daily sum
         *dailyValue += readings[i];
     }
 
+    //  Calculate the cost
     *dailyCost = (*dailyValue) * COST_KWH;
     
+    // Print everything
     printf("    -> Peak Consumption: %.2f kW/h\n", *peak);
     printf("    -> Total Daily Consumption: %.2f kW/h\n", *dailyValue);
     printf("    -> Total Cost: %.2f EUR\n", *dailyCost);
--- main.c
+++ main.c
@@ -1,6 +1,27 @@
+/** \file main.c
+*   \brief Main test file for the Energy Consumption module
+*
+*        This file tests all the funtions on the 
+*       Energy Consumption module developped for this class
+*
+* \author Pedro Ramos 
+* \author Rafael Morgado
+* \date 18/2/2025
+* \bug There are no bugs
+*/
+
 #include <stdio.h>
 #include "enerconsumption.h"
 
+/**
+ * \brief Main function to test the Energy Consumption module.
+ *
+ * This function initializes the module, adds energy readings, 
+ * and calculates the total energy consumption, peak usage, 
+ * and cost.
+ *
+ * \return Returns 0 upon successful execution.
+ */
 int main(void) {
     
     printf(" - == === Testing initialization === == -\n");
